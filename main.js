116c79f9f3db66cdc1006fa36ff387a50c6b08d6
Brian Kenji Aguilar <anjacular@gmail.com>
2018-08-30 20:41:59 -0400

    Added Commets to sample code for IPC (later to be used) and added a template menu

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const app = electron.app;
 const BroswerWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
+const Menu = electron.Menu;
+const ipc = electron.ipcRenderer;
 
 let window;
 
 function createWindow() {
-    window = new BroswerWindow({title:"Media Title"});
+    window = new BroswerWindow({title:"Media Player"});
+
     window.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
@@ -19,4 +22,60 @@ function createWindow() {
     });
 }
 
-app.on('ready', createWindow);
+/**
+ * ipc.on('event_name', function(event){
+ *  Have a function arguement to tell the main 
+ *  process what to do when the render process
+ *  sends for a request
+ * 
+ *  To send back a response you write
+ *  the event name must be the same
+ *  event.sender.send('event_name', response);
+ * })
+ */
+
+ /**
+  * To active ipc on the render process you write this code
+  *  
+  * import electron and the ipc
+  * 
+  * HTML.addEventListener('click',function(){
+  *     ipc.send('event_name');
+  * });
+  * 
+  * ipc.on('event_name',function(event,arg){
+  *     the argument is the response
+  *     console.log(arg);
+  * });
+  */
+
+app.on('ready', function(){
+    createWindow();
+    /**
+     * Creating the menu after creating
+     * the window
+     */
+    const template = [
+        {
+            label: 'File',
+            submenu:[
+                {
+                    label:'Add Song'
+                },
+                {
+                    label:'Exit',
+                    click: function(){
+                        window.close();
+                    }
+                }
+            ]
+        },
+        {
+            label: 'Help'
+        }
+    ];
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+    
+});
